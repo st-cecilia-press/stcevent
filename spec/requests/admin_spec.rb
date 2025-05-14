d1079de7637e23a8377455dedc8255dfad3f8e37
Monique Rio <mrio@umich.edu>
2025-05-14 17:15:50 +0000

    adds sign out button

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe "/", type: :request do
     create(:page, slug: "home")
   end
   it "shows admin menu item for a logged in user" do
-    user = create(:user)
     get root_url as: user
     expect(response.body).to include("Admin")
   end
   it "does not show a menu item for an anonymous user" do
     get root_url
     expect(response.body).not_to include("Admin")
+  end
+  it "has a Sign out link in the admin dropdown" do
+    get root_url as: user
+    expect(response.body).to include("Sign out")
   end
 end
