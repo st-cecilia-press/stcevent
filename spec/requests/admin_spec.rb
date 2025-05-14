63c2a7ee92bf29e79492e973cf2a8b6643098561
Monique Rio <mrio@umich.edu>
2025-05-14 17:03:03 -0400

    formatting on forms; adds flash messages
    * changes form-group to mb-3
    * creates a custom primary button
    * adds the view for a new session
    * adds a method for generating message css class from flash type

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "/", type: :request do
   let(:user) { create(:user) }
-  before(:all) do
+  before(:each) do
     create(:page, slug: "home")
   end
   it "shows admin menu item for a logged in user" do
@@ -16,5 +16,13 @@ RSpec.describe "/", type: :request do
   it "has a Sign out link in the admin dropdown" do
     get root_url as: user
     expect(response.body).to include("Sign out")
+  end
+end
+
+RSpec.describe "/sign_in", type: :request do
+  it "shows a flash error when failing to sign in" do
+    create(:page, slug: "home")
+    post session_url, params: {session: {email: "nobdy@donotexist.invalid", password: "invalidpassword"}}
+    expect(response.body).to include("Bad email or password.")
   end
 end
