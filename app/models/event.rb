8b2c2d55e4653068e81cf25610a8537262282536
Aaron Elkiss <aelkiss@umich.edu>
2025-06-02 14:43:12 -0400

    Test and fix schedule display bug
    * move json stuff to models where it's easier to test (ultimately
      belongs in a presenter)
    * show hours & minutes, not hours & month

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,13 @@ class Event < ActiveRecord::Base
 
   # Current event is the most recent (even if the most recent is in the future)
   scope :current, -> { order(start_date: :desc).limit(1).first }
+
+  def classrooms_json
+    classrooms.map do |classroom|
+      {
+        id: classroom.id,
+        title: classroom.name
+      }
+    end.to_json.html_safe
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,4 +6,15 @@ class Schedule < ActiveRecord::Base
   def ordered_entries
     entries.includes(:classroom).order(:start_time, "classroom.name")
   end
+
+  def entries_json
+    entries.map do |entry|
+      {
+        start: entry.start_time.strftime("%Y-%m-%d %H:%M"),
+        end: entry.end_time.strftime("%Y-%m-%d %H:%M"),
+        resourceId: entry.classroom_id,
+        title: {html: %(<a href="#entry_#{entry.id}">#{entry.title}</a>)}
+      }
+    end.to_json.html_safe
+  end
 end
